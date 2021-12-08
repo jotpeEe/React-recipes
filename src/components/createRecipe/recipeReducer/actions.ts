1b31b9a9f00ef7777909044208627f1def33fe71
mac <maciekklospl@gmail.com>
2021-12-08 05:12:41 +0000

    imp-feature: added global const ACTIONS for good practice

---
+++ src/components/createRecipe/recipeReducer/actions.ts
@@ -0,0 +1,19 @@
+export interface ActionsProps {
+  ADD_INGREDIENT: string;
+  EDIT_INGREDIENT: string;
+  ADD_DESCRIPTION: string;
+  DELETE_INGREDIENT: string;
+  SET_TITLE: string;
+  SET_STEP: string;
+}
+
+export const ACTIONS: ActionsProps = {
+  ADD_INGREDIENT: 'add-ingredient',
+  ADD_DESCRIPTION: 'add-description',
+  EDIT_INGREDIENT: 'edit-ingredient',
+  DELETE_INGREDIENT: 'delete-ingredient',
+  SET_TITLE: 'set-title',
+  SET_STEP: 'set-step',
+};
+
+export default ACTIONS;
