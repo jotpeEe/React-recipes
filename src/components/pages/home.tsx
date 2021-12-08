668bcf3eec5f279034301f7c45b726deb4e489a2
mac <maciekklospl@gmail.com>
2021-12-08 05:06:10 +0000

    clean-up: removed old components, used for useReducer flow

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import GlobalStyle from '../../styles/globalstyle';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../styles/theme';
-import Add from './createRecipe';
+import CreateRecipe from './createRecipe';
 
 function Home() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Add />
+        <CreateRecipe />
       </ThemeProvider>
     </>
   );
