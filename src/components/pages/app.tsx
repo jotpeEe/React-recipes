220902b5d201801b4ad4a39c4bcc88d078684d6a
mac <maciekklospl@gmail.com>
2022-03-12 22:15:10 +0000

    featrue: add axios to the project

--- src/components/pages/app.tsx
+++ src/components/pages/app.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import GlobalStyle from '../../styles/globalstyle';
